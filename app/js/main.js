efe0f3cdd7cd5b65cc770547e8dca0735702716b
Michael Marcinek <michaelmarcinek@gmail.com>
2015-06-23 13:47:21 -0400

    Finishes easy mode

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,25 @@
-console.log('This is cool!');
+;(function (){
 
-$('.container').html(template.example({
-  value: 'Hello, World!!'
-}));
+  'use strict';
+
+  angular.module('ButtonModule', [])
+
+  .controller('ButtonController', ['$scope', function ($scope){
+
+    $scope.likes = [];
+
+    $scope.likes= 0;
+
+    $scope.count = function (){
+      return $scope.likes;
+    };
+
+    $scope.addLike = function incrementScope(){
+      $scope.likes ++;
+
+      console.log($scope.likes);
+    };
+
+  }]);
+
+}());
