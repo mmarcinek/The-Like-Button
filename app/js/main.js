a4ae6dcf7b40891d9a27699e5c122e80da32d4c2
Michael Marcinek <michaelmarcinek@gmail.com>
2015-06-23 14:50:59 -0400

    Changes plurality of like

--- app/js/main.js
+++ app/js/main.js
@@ -14,10 +14,19 @@
       return $scope.likes;
     };
 
+    $scope.plural = function () {
+      if($scope.likes === 1){
+        return 'like';
+      } else {
+        return 'likes';
+      }
+    };
+
+
     $scope.addLike = function incrementScope(){
       $scope.likes ++;
 
-      console.log($scope.likes);
+      // console.log($scope.likes);
     };
 
   }]);
